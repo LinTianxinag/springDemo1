5bf3284406626e5a87bd4c8e27d3adc79c0c0ef1
LinTianxinag <1152453141@qq.com>
2018-06-26 14:59:30 +0800

    wxchang1

--- web/src/main/java/com/MyDemo/web/WXController.java
+++ web/src/main/java/com/MyDemo/web/WXController.java
@@ -6,6 +6,10 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
 /**
  * desciper::介绍方法名
  * Created by LinTianxiang.
@@ -17,7 +21,24 @@ public class WXController {
 
     @RequestMapping(value = "/wx")
     @ResponseBody
-    public String home(ModelMap map, User user){
-        return "hello, this is wx view";
+    public Object home(String signature, String timestamp, String nonce, String echostr){
+        String token = "3qOCpeh2vimfssLirU";
+        String[] strs=new String[] {token,timestamp,nonce};
+        Arrays.sort(strs);
+        StringBuffer content=new StringBuffer();
+        for (int i = 0; i < strs.length; i++) {
+            content.append(strs[i]);
+        }
+        MessageDigest md = null;
+        String tmpStr = null;
+        try {
+        md = MessageDigest.getInstance("SHA-1");
+            // 将三个参数字符串拼接成一个字符串进行sha1加密
+            byte[] digest = md.digest(content.toString().getBytes());
+            tmpStr = new String(digest);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return signature!=null?signature.toUpperCase().equals(tmpStr.toUpperCase()):false;
     }
 }
